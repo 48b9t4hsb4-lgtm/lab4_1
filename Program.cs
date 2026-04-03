31ef6acfea098b6949ee3bfe47fc7abd016c643d
n3r3v1 <annakurochkina@icloud.com>
2026-04-04 01:43:07 +0300

    try 1

---
+++ Program.cs
@@ -0,0 +1,55 @@
+﻿public class Flower
+{
+    public string Name { get; set; }
+
+    public Flower(string name)
+    {
+        Name = name;
+    }
+
+    public void Blossom()
+    {
+        Console.WriteLine($"{Name} розквітає");
+    }
+
+    public void Wither()
+    {
+        Console.WriteLine($"{Name} зів'яла");
+    }
+
+}
+
+public class Petal
+{
+    public string Color { get; set; }
+    public Petal(string color)
+    {
+        Color = color;
+    }
+
+}
+
+public class Bud
+{
+    public string Color { get; set; }
+    public Bud(string color)
+    {
+        Color = color;
+    }
+}
+
+public class Hydrangea : Flower
+{
+    public Petal petal;
+    public Bud bud;
+
+    public Hydrangea(string Name, Petal petal, Bud bud) : base(Name)
+    {
+        this.petal = petal;
+        this.bud = bud;
+
+    }
+}
+
+
+
